d4ddafee86a938b6cd366b095086dd554d153da8
Nicholas Sobchak <nsob79@gmail.com>
2025-11-28 21:04:40 +0000

    class initilization

--- Dictionary.h
+++ Dictionary.h
@@ -1,6 +1,27 @@
 #ifndef DICTIONARY_H
 #define DICTIONARY_H
+#include <iostream>
+#include <string>
+using namespace std;
+class Tester;
 
-// implement dictionary.h here
+class Dictionary
+{
+public:
+    friend class Tester;
+    // implement Dictionary class here
+
+private:
+    // private members here
+};
+
+class SpellChecker
+{
+public:
+    // implement SpellChecker class here
+
+private:
+    // private members here
+};
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 // Fall 2025 - Dictionary/Spell Checker - Collaborative Project
-
 #include <iostream>
 #include "Dictionary.h"
-// implement main here
+using namespace std;
+
+class Tester
+{
+};
+
+int main()
+{
+
+    return 0;
+};
